cc79fc6e8beac016c084648fbcc8acb7810ec3af
dslima18 <donatosouza33@gmail.com>
2024-01-23 16:02:24 -0300

    APIS
    Neste capítulo aprendemos a usar APIs para escrever programas
    autocontidos que coletem automaticamente os dados necessários e
    usem esses dados para criar uma visualização. Usamos a API do GitHub
    para explorar os projetos Python com mais estrelas no GitHub e vimos
    rapidamente a API do Hacker News também. Aprendemos a usar o
    pacote requests para fazer uma chamada de API ao GitHub de modo
    automático e a processar os resultados dessa chamada. Também
    apresentamos algumas configurações do Pygal para personalizar melhor
    a aparência dos gráficos que você gerar.
    No último projeto, usaremos Django para criar uma aplicação web.

---
+++ Capítulo 17/python_repos.py
@@ -0,0 +1,52 @@
+import requests
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
+# Faz uma chamda De API e armazena a resposta
+url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+r = requests.get(url)
+print('Status code:', r.status_code)
+# Armazena a resposta da API em uma variável
+responde_dict = r.json()
+print("Total repositories:", responde_dict['total_count'])
+# Explora as informações sobre os repositórios
+repo_dicts = responde_dict['items']
+names, plot_dicts = [], []
+
+# Analisa o primeiro repositório
+#repo_dict = repo_dicts[0]
+
+#print("\nKeys:", len(repo_dict))
+#print("\nSelected information about first repository:")
+for repo_dict in repo_dicts:
+    names.append(repo_dict['name'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        'xlink': repo_dict['html_url'],
+    }
+    plot_dicts.append(plot_dict)
+# Cria a visualização
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.title_font_size = 24
+my_config.label_font_size = 14
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+my_style = LS('#333366', base_style=LCS)
+chart = pygal.Bar(my_config, style=my_style)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos.svg')
+    #print('Name:', repo_dict['name'])
+    #print('Owner:', repo_dict['owner']['login'])
+    #print('Stars:', repo_dict['stargazers_count'])
+    #print('Repository:',repo_dict['html_url'])
+    #print('Description:', repo_dict['description'])
+#for key in sorted(repo_dict.keys()):
+#    print(key)
+# PRocessa o resultado
+#print(responde_dict.keys())
